7232a055af4b1e44f974f1a1b9d232cd3da8d597
Yahaya <yasholma1@gmail.com>
2020-07-01 18:55:08 +0100

    Add Anim

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,7 @@
 $(window).on('load', function () {
   $('#status').fadeOut()
   $('#preloader').delay(500).fadeOut()
+  $('.navbar-brand').addClass('animate__animated animate__bounce')
 })
 
 // Slick Slider Configuration
