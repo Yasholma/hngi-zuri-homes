2ecbbcb698d501dda409c576cdd76907c7efbec3
Yahaya <yasholma1@gmail.com>
2020-07-06 13:53:16 +0100

    add zoom effect to residence page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -43,3 +43,33 @@ $(document).ready(function () {
 $(function () {
   new WOW().init()
 })
+
+// Magnific POP UP
+$(function () {
+  // Residence Main Images
+  $('.residence-main-container').magnificPopup({ delegate: 'a', type: 'image' })
+
+  // Residence Child Images //
+  // Residence 1
+
+  // Residence 2
+  magnifyImages('residence-1')
+  magnifyImages('residence-2')
+  magnifyImages('residence-3')
+  magnifyImages('residence-4')
+  magnifyImages('residence-5')
+  magnifyImages('residence-6')
+  magnifyImages('residence-7')
+  magnifyImages('residence-8')
+  magnifyImages('residence-9')
+})
+
+function magnifyImages(id) {
+  $(`#${id}`).magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true,
+    },
+  })
+}
